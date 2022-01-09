c1da7ae35aea263818fc0040f6230574712c3c36
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-01-09 10:00:25 +0330

    Using most used tags and etc

--- resources/config/external.php
+++ resources/config/external.php
@@ -2,15 +2,20 @@
 
 function cssMaterial() {
     ?>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.1/mdb.min.css" rel="stylesheet">
     <?php
 }
 
-function jsMaterial() {
+function jsMaterial($home) {
     ?>
+    <script src="<?php echo $home; ?>/resources/js/script.js"></script>
+    <script src="<?php echo $home; ?>/resources/js/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.1/mdb.min.js"></script>
     <?php
 }
 
 function fontAwesome() {
     ?>
+    <script src="https://kit.fontawesome.com/4a679d8ec0.js" crossorigin="anonymous"></script>
     <?php
 }

bf8b06aea1530cf42358379677fb1728d1a6d128
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-01-09 20:38:59 +0330

    Update navbar

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+session_start();
+
+include('resources/config/config.php');
+
+if (isset($_SESSION['status'])) {
+    $session_status = false;
+}
+else {
+    $session_status = true;
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>صفحه اصلی</title>
+    <script src="https://kit.fontawesome.com/4a679d8ec0.js" crossorigin="anonymous"></script>
+    <link href="<?php echo $home; ?>/resources/css/main.css" type="text/css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.1/mdb.min.css" rel="stylesheet">
+</head>
+<body class="body">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">دکوراسیون ایران زمین</a>
+        <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarText"
+                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">خانه</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">درباره ما</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">سوالات متداول</a>
+                </li>
+            </ul>
+            <div class="me-auto">
+                <?php
+                if ($session_status == true) {
+                    echo '<a href="client" class="nav-link">ورود به حساب</a>';
+                } else {
+                    echo '<a href="client/logout.php" class="nav-link">خارج شدن از حساب</a>';                }
+                ?>
+            </div>
+        </div>
+    </div>
+</nav>
+
+<script src="<?php echo $home; ?>/resources/js/script.js"></script>
+<script src="<?php echo $home; ?>/resources/js/jquery-3.6.0.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.1/mdb.min.js"></script>
+</body>
+</html>

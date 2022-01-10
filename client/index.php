169a64ded9e4273d0d8abb29c4434a57608c0281
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-01-10 14:36:17 +0330

    Add handle user status session

--- client/index.php
+++ client/index.php
@@ -7,6 +7,10 @@ $session_status = $_SESSION['status'];
 include('../resources/config/config.php');
 include('../resources/routes/route.php');
 
+if ($session_status == true) {
+    header('Location: ' . $home . '/user');
+}
+
 ?>
 
 <!doctype html>
--- user/index.php
+++ user/index.php
@@ -7,7 +7,7 @@ $session_status = $_SESSION['status'];
 include('../resources/config/config.php');
 include('../resources/routes/route.php');
 
-if ($session_status) {
+if ($session_status == true) {
     echo $_SESSION['user']['name'];
 }
 else {

8d0baacf9b8e5610b59e0a5c329dbb4777b87e18
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-01-12 13:26:42 +0330

    Start to work on it

--- user/index.php
+++ user/index.php
@@ -7,9 +7,31 @@ $session_status = $_SESSION['status'];
 include('../resources/config/config.php');
 include('../resources/routes/route.php');
 
-if ($session_status == true) {
-    echo $_SESSION['user']['name'];
-}
-else {
+if (!$session_status) {
     header('Location: ' . $home . '/client');
 }
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>داشبورد کاربر</title>
+    <script src="https://kit.fontawesome.com/4a679d8ec0.js" crossorigin="anonymous"></script>
+    <link href="<?php echo $home; ?>/resources/css/main.css" type="text/css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.1/mdb.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="hi">
+    <h1>H</h1>
+</div>
+
+<script src="<?php echo $home; ?>/resources/js/jquery-3.6.0.min.js"></script>
+<script src="<?php echo $home; ?>/resources/js/script.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.1/mdb.min.js"></script>
+</body>
+</html>

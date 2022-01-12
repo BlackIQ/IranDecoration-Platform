6989c1eddb0514da10cc51eee766f4f2cd1e152b
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-01-12 23:53:40 +0330

    Update values

--- resources/config/config.php
+++ resources/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 // Create MySQL connection
-$connection = mysqli_connect('localhost', 'amir', 'amir', 'fardin');
+$connection = mysqli_connect('localhost', 'amir', 'amir', 'irandecoration');
 
 // Add home variable
-$home = 'http://192.168.1.4/fardin';
+$home = 'http://192.168.1.4/irandecoration';
